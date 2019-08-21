6096b626eee497f39e09db0e8cbd215249e82d56
Tobse <Tobse4Git@gmail.com>
2019-08-21 11:38:18 +0200

    🔨 Make `HueApiException` public

--- src/main/java/io/github/zeroone3010/yahueapi/HueApiException.kt
+++ src/main/java/io/github/zeroone3010/yahueapi/HueApiException.kt
@@ -1,6 +1,6 @@
 package io.github.zeroone3010.yahueapi
 
-internal class HueApiException : RuntimeException {
+class HueApiException : RuntimeException {
   constructor(cause: Throwable) : super(cause)
 
   constructor(message: String) : super(message)
